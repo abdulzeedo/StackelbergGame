9282a08685b975f4ca94c4975971d906e219ec8a
Matthew Spear <matthewtimothyspear@gmail.com>
2019-05-04 21:07:16 +0100

    Add MovingWindowLeader class and scripts
    MovingWindowLeader is a copy of SimpleLeader where we will add our
    modified code. Scripts are provided for building and running the leader.

---
+++ build-movingwindowleader.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Build our leader
+javac src/MovingWindowLeader.java 
+
+cp src/MovingWindowLeader.class .
+
---
+++ run-movingwindowleader.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# run the MovingWindowLeader
+java -Djava.rmi.server.hostname=127.0.0.1 MovingWindowLeader &
+
